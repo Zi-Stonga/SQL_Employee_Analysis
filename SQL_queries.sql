f44e3e4b3e6a3fd121611f67e3b3cdad9b048773
Zi-Stonga <62084056+Zi-Stonga@users.noreply.github.com>
2023-08-01 13:55:43 -0400

    Initial Tables

---
+++ SQL_queries.sql
@@ -0,0 +1,8 @@
+CREATE TABLE locations
+   ( location_id NUMERIC(4),
+   street_address TEXT(40),
+   postal_code NUMERIC(12),
+   city TEXT(30),
+   state_province TEXT(25),
+   country_id TEXT(2)
+   ) ;
